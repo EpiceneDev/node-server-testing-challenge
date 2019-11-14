d868e280d94be2ab604a581504731abf0bb453c2
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2019-11-14 15:01:28 -0800

    added async via jest to server test

--- api/server.spec.js
+++ api/server.spec.js
@@ -5,9 +5,11 @@ const server = require('./server.js');
 describe('server', function() {
     describe('GET /', function() {
         it('should return 200 OK', function() {
-            request(server).get('/').then(res => {
+            return request(server)
+                .get('/')
+                .then(res => {
                 expect(res.status).toBe(200);
-            });
+                });
         });
     });
 });
